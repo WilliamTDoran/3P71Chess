c315dcef3f089606e495212d7d1545d5113bc0dc
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2022-12-19 20:46:05 -0500

    Button things
    Butt stuff

--- ChessUnity/3P71Chess/Assets/Scripts/Chess Game/BoardInitializer.cs
+++ ChessUnity/3P71Chess/Assets/Scripts/Chess Game/BoardInitializer.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardInitializer : MonoBehaviour
 {
     internal bool custom = false;
+
+    [SerializeField]
+    private Button customButton;
+    [SerializeField]
+    private Button backButton;
+    [SerializeField]
+    private GameObject configMenu;
 
     short[,] defaultConfiguration = new short[,] { {  -50,  -30,  -32,  -90, -900,  -32,  -30,  -50 },
                                                    {  -10,  -10,  -10,  -10,  -10,  -10,  -10,  -10 },
@@ -24,4 +32,20 @@ public class BoardInitializer : MonoBehaviour
 
         return null;
     }
+
+
+    public void EnableCustom()
+    {
+        custom = true;
+        customButton.gameObject.SetActive(false);
+        configMenu.SetActive(true);
+    }
+
+    public void DisableCustom()
+    {
+        custom = false;
+        customButton.gameObject.SetActive(true);
+        configMenu.SetActive(false);
+    }
+
 }
